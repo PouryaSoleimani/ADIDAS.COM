8a154f1f62b26a31a3d5963e17de98668de146ed
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-08 20:09:26 +0330

    REFACTOR:

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 // ^ HOME __________________________________________________________________________________________________________
 import React, { useEffect } from 'react'
 import Header from '../Components/Header/Header'
