5453f7f4bdac2a315db15c05590c9ef80a6ffdb1
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-08 20:17:44 +0330

    REFACTOR:

--- src/Components/HomePageVideo/HomePageVideo.jsx
+++ src/Components/HomePageVideo/HomePageVideo.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useRef } from 'react'
 import { FaLongArrowAltRight } from 'react-icons/fa'
 import './HomePageVideo.css'
@@ -10,14 +11,12 @@ const HomePageVideo = () => {
  const vid = useRef()
  useEffect(() => { vid.current.play() }, [])
  return (
-  <div className='mt-8'>
-   <div>
-    <video autoPlay loop muted src="/videos/homepage_video.mp4" className='relative left-0 top-0 w-full' ref={vid}></video>
-    <div className='-translate-y-[9.6rem]  sm:-translate-y-[10rem]  md:-translate-y-[15rem] flex flex-col space-y-2 md:space-y-4 px-2 md:px-8'>
+  <div className='mt-8 h-fit'>
+    <video autoPlay loop muted src="/videos/homepage_video.mp4" className='relative scale-y-125 lg:scale-y-100 left-0 top-0 w-full' ref={vid}></video>
+    <div className='-translate-y-[11rem] px-4 tracking-tight  sm:-translate-y-[10rem]  md:-translate-y-[15rem] flex flex-col space-y-2 md:space-y-4 md:px-8'>
      <h1 className=' z-10   text-slate-100 font-bold text-2xl md:text-5xl tracking-tighter font-sans'>2024 NATIONAL TEAMS SOCCER KITS</h1>
      <h3 className=' z-10  text-slate-100 text-md  md:text-2xl font-semibold tracking-tighter font-sans'>Time to show your Colors and gear up for a summer of colors</h3>
      <button className='button z-10 w-[10rem] font-bold h-12 md:h-16 px-6 md:py-3'><Link to='/products'> <span className="text flex items-center gap-x-2 ">Shop Now<FaLongArrowAltRight /></span> <span className='text flex items-center gap-x-1'>SHOP</span>   </Link> </button>
-    </div>
    </div>
   </div>
  )
--- src/Components/NationalKits/NationalKits.jsx
+++ src/Components/NationalKits/NationalKits.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const NationalKits = () => {
   return (
     <div>
-      <img src="/images/NationalKitsGif.gif" className='w-screen relative z-0' />
+      <img src="/images/NationalKitsGif.gif" className='w-screen relative z-0 ' />
       <div className='flex justify-end h-full flex-col pl-8 md:px-10 py-4 z-30 -translate-y-[8rem]  sm:-translate-y-[8rem]   md:-translate-y-[10rem] space-y-2'>
         <h1 id='newCollection' className='text  text-black text-xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold tracking-tighter leading-tight  z-30' >NATIONAL GAMES ARE ABOUT TO<span className='bg-red-600 text-white ml-2'>START</span> </h1>
         <button className='button text-5xl font-extrabold tracking-tighter leading-tight  w-[10rem] z-30'><Link to='/products/jerseys'>LET'S GO ! <span>LET'S GO !</span></Link></button>
