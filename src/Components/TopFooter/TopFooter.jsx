f5903e663b753649d27be15dc9e347f9520f10d5
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-11-23 20:04:57 +0330

    NEW___REVIEW

--- src/Components/TopFooter/TopFooter.jsx
+++ src/Components/TopFooter/TopFooter.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 
 const TopFooter = () => {
- return (
-  <div className='bg-zinc-200 hidden md:flex w-full h-[37rem] mt-0  px-16 md:py-10 lg:py-16 items-center justify-center space-x-10 tracking-tighter leading-6'>
+  return (
+    <div className="bg-zinc-200 hidden md:flex w-full h-[37rem] mt-0  px-16 md:py-10 lg:py-16 items-center justify-center space-x-10 tracking-tighter leading-6">
+      <div className="flex flex-col px-4 space-y-8">
+        <h1 className="text-2xl font-bold">Stories, style, and sporting goods at adidas, since 1949</h1>
+        <p className="whitespace-pre-wrap w-3/4">
+          Through sports, we have the power to change lives. Sports keep us fit. They keep us mindful. They bring us together. Athletes inspire us.
+          They help us to get up and get moving. And sporting goods featuring the latest technologies help us beat our personal best. adidas is home
+          to the runner, the basketball player, the soccer kid, the fitness enthusiast, the yogi. And even the weekend hiker looking to escape the
+          city. The 3-Stripes are everywhere and anywhere. In sports. In music. On life’s stages. Before the whistle blows, during the race, and at
+          the finish line. We’re here to support creators. To improve their game. To live their lives. And to change the world.
+        </p>
+      </div>
 
-   <div className='flex flex-col px-4 space-y-8'>
-    <h1 className='text-2xl font-bold'>Stories, style, and sporting goods at adidas, since 1949</h1>
-    <p className='whitespace-pre-wrap w-3/4'>Through sports, we have the power to change lives. Sports keep us fit. They keep us mindful. They bring us together. Athletes inspire us. They help us to get up and get moving. And sporting goods featuring the latest technologies help us beat our personal best. adidas is home to the runner, the basketball player, the soccer kid, the fitness enthusiast, the yogi. And even the weekend hiker looking to escape the city. The 3-Stripes are everywhere and anywhere. In sports. In music. On life’s stages. Before the whistle blows, during the race, and at the finish line. We’re here to support creators. To improve their game. To live their lives. And to change the world.
-    </p>
-   </div>
-
-   <div className='flex flex-col px-4 space-y-8'>
-    <h1 className='text-2xl font-bold '>Workout clothes, for any sport</h1>
-    <p className='whitespace-pre-wrap w-3/4'>adidas designs for athletes of all kinds. Creators who love to change the game. People who challenge conventions, break the rules, and define new ones. Then break them all over again. We design sports apparel that gets you moving, winning, and living life to the fullest. We create bras and tights for female athletes who play just as hard as the men. From low to high support. Maximum comfort. We design, innovate and iterate. We test new technologies in action. On the field, the track, the court, and in the pool. We’re inspired by retro workout clothes, creating new streetwear essentials. From NMD and Ozweego to our Firebird tracksuits. From Stan Smith to Superstar. Classic sports models are brought back to life on the streets and the stages around the world.
-    </p>
-   </div>
-
-  </div>
- )
+      <div className="flex flex-col px-4 space-y-8">
+        <h1 className="text-2xl font-bold ">Workout clothes, for any sport</h1>
+        <p className="whitespace-pre-wrap w-3/4">
+          adidas designs for athletes of all kinds. Creators who love to change the game. People who challenge conventions, break the rules, and
+          define new ones. Then break them all over again. We design sports apparel that gets you moving, winning, and living life to the fullest. We
+          create bras and tights for female athletes who play just as hard as the men. From low to high support. Maximum comfort. We design, innovate
+          and iterate. We test new technologies in action. On the field, the track, the court, and in the pool. We’re inspired by retro workout
+          clothes, creating new streetwear essentials. From NMD and Ozweego to our Firebird tracksuits. From Stan Smith to Superstar. Classic sports
+          models are brought back to life on the streets and the stages around the world.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default TopFooter;
